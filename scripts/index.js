32879a366dcb4d0f8a4924ae9d6ebb6e65063f2b
NGOCHAI <phamngochai0209@gmail.com>
2024-05-04 15:46:08 +0900

    a

--- scripts/index.js
+++ scripts/index.js
@@ -7,16 +7,3 @@ document.addEventListener('DOMContentLoaded', function() {
       navMenu.classList.toggle('active');
     });
   });
-
-// // JavaScript để thêm sự kiện click cho nút menu toggle
-// document.addEventListener('DOMContentLoaded', function() {
-//     const menuToggle = document.querySelector('.menu-toggle');
-    
-//     menuToggle.addEventListener('click', function() {
-//       // Toggle lớp 'active' cho nút menu toggle
-//       menuToggle.classList.toggle('active');
-//     });
-//   });
-  
-
-  
